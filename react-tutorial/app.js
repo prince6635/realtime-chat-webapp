11909d36b93349c3b0b1ee766542d55cbd6c1189
Zi Wang <ziwang@uber.com>
2017-04-07 15:09:14 -0700

    React events binding

--- react-tutorial/app.js
+++ react-tutorial/app.js
@@ -1,8 +1,14 @@
 // https://babeljs.io/ -> "Try it out": convert the following JSX code to normal JS function
 class Channel extends React.Component{
+
+  onClick(){
+    console.log('I was clicked', this.props.name);
+  }
+
   render(){
     return(
-      <li>{this.props.name}</li>
+      // need to .bind(this), so inside onClick, it can access this.xxx
+      <li onClick={this.onClick.bind(this)}>{this.props.name}</li>
     )
   }
 }
